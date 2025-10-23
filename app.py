383bc154ce9d65f50c02088009b306e470de51e8
gsalgado2904-bit <gsalgado2904@gmail.com>
2025-10-23 12:02:54 -0500

    Refactor file path and improve button functionality

--- app.py
+++ app.py
@@ -1,40 +1,40 @@
 import pandas as pd
-     import plotly.express as px
-     import streamlit as st
+import plotly.express as px
+import streamlit as st
 
-     car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-     car_data["manufacturer"] = car_data['model'].str.split(" ").str[0]
-     type_brand_button = st.button("Construir comparativa de marca con tipo de vehiculo")
-     hist_button = st.checkbox('Construir histograma') # crear un botón
-     scatt_button = st.checkbox("Construir grafico de dispersion")
+car_data = pd.read_csv('/content/drive/My Drive/Colab Notebooks/Triple ten/Sprint 7/vehicles_us.csv') # leer los datos
+car_data["manufacturer"] = car_data['model'].str.split(" ").str[0]
+type_brand_button = st.button("Construir comparativa de marca con tipo de vehiculo")
+hist_button = st.checkbox('Construir histograma') # crear un botón
+scatt_button = st.checkbox("Construir grafico de dispersion")
       
-     st.header("Vehicle graphics and analysis")
+st.header("Vehicle graphics and analysis")
 
-     if type_brand_button:
-         st.write("Construir comparativa de marca con tipo de vehiculo")
+if type_brand_button:
+    st.write("Construir comparativa de marca con tipo de vehiculo")
 
-         fig_manufacturer_type = fig = px.histogram(car_data, x="manufacturer", color="type", title="Vehicle type and brand")
+    fig_manufacturer_type = fig = px.histogram(car_data, x="manufacturer", color="type", title="Vehicle type and brand")
 
-         st.plotly_chart(fig_manufacturer_type,use_container_width=True)
+    st.plotly_chart(fig_manufacturer_type,use_container_width=True)
 
 
 
-     if hist_button: # al hacer clic en el botón
-         # escribir un mensaje
-         st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
-         # crear un histograma
-         fig_model_condition = px.histogram(car_data, x="model_year", color="condition", title="Vehicle Model vs. Condition")
+    # crear un histograma
+    fig_model_condition = px.histogram(car_data, x="model_year", color="condition", title="Vehicle Model vs. Condition")
 
-         # mostrar un gráfico Plotly interactivo
-         st.plotly_chart(fig_model_condition, use_container_width=True)
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_model_condition, use_container_width=True)
 
-     if scatt_button: # al hacer clic en el botón
-         # escribir un mensaje
-         st.write('Creacion de un diagrama de dispersion del precio comparado con el millaje')
+if scatt_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creacion de un diagrama de dispersion del precio comparado con el millaje')
 
-         # crear un histograma
-         fig_price_mileage = px.scatter(car_data, x="odometer", y="price", color="condition", log_y=True, title="Vehicle Price vs. Mileage")
+    # crear un histograma
+    fig_price_mileage = px.scatter(car_data, x="odometer", y="price", color="condition", log_y=True, title="Vehicle Price vs. Mileage")
 
-         # mostrar un gráfico Plotly interactivo
-         st.plotly_chart(fig_price_mileage, use_container_width=True)
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_price_mileage, use_container_width=True)
